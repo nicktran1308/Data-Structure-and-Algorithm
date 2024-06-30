ddc4c9eddf845cb3a8d9b83fb6a47196b175818a
Nick Tran <123424016+nicktran1308@users.noreply.github.com>
2024-06-29 21:02:40 -0700

    Create is_subsequence.py

---
+++ data_structure/array_hashing/is_subsequence.py
@@ -0,0 +1,52 @@
+"""
+392. Is Subsequence
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+ 
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+"""
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
+
+
+"""
+Time complexity: O(n) - n: length of string t
+Space complexity: O(1) - the algorithm uses a fixed amount of space regardless of the input size
+
+Steps:
+    1. Create two pointers, i and j
+    2. While i < len(s) and j < len(t):
+    3. If at a certain point, s[i] == t[j], increment i ( string s)
+    4. Increment j ( string t)
+    5. Return True if i == len(s)
+
+"""
